2255410c682d08de7f0a7dd412f7c339d1b3375b
Filipe F. Kalicki <filipe.kalicki@gmail.com>
2018-04-26 02:02:48 -0300

    Updated mount /api

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative '../lib/.'
 require_relative '../apps/api/application'
 
 Hanami.configure do
-  mount Api::Application, at: '/'
+  mount Api::Application, at: '/api'
 
   model do
     ##
