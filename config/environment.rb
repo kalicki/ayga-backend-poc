8de9814bca55f0b5ba03d7d83b07c403a31b1fbe
Filipe F. Kalicki <filipe.kalicki@gmail.com>
2018-04-26 01:49:40 -0300

    Add support for Puma and HTTP/2

---
+++ config/environment.rb
@@ -0,0 +1,52 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/.'
+require_relative '../apps/api/application'
+
+Hanami.configure do
+  mount Api::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/._development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/._development'
+    #    adapter :sql, 'mysql://localhost/._development'
+    #
+    adapter :sql, ENV.fetch('DATABASE_URL')
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/./mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: http://hanamirb.org/guides/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json, filter: []
+
+    mailer do
+      delivery :smtp, address: ENV.fetch('SMTP_HOST'), port: ENV.fetch('SMTP_PORT')
+    end
+  end
+
+  # Support to HTTP/2
+  early_hints true
+end
